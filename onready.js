b45ebc8376251b6a16841413bcaffd8423ec15da
Valentin Hilbig <valentin.hilbig@sol-venetus.de>
2019-08-30 11:53:27 +0200

    onready.js modified

--- onready.js
+++ onready.js
@@ -1,14 +1,24 @@
-// <div id="main">Please enable JavaScript!  Bitte JavaScript einschalten!</div>
-// <script src="onready.js"></script></body></html> ensures:
-// - this is the last thing loaded
-// - DOM is ready
+///STANDALONE
 // Based on an idea presented in https://gist.github.com/shesek/441c02cf1f094c36f57a
+//
+// ;onready=[onready, YOUR_INIT_FUNCTION];
+//
+// If HTML is written this way, it ensures, that YOUR_INIT_FUNCTION is called when the DOM is ready:
+//
+// <!DOCTYPE html>
+// <html><head>
+// <meta charset="utf-8"/>
+// ..
+// <script type="text/javascript" src="js/loader.js"></script>
+// </head><body>
+// <div id="main">Please enable JavaScript!  Bitte JavaScript einschalten!</div>
+// ..
+// <script src="js/onready.js"></script></body></html>
 
 ;(function(){
-  var list=window.onready;
+  var list=Array.prototype.slice.call([window.onready]);
   window.onready = { push: function(fn) { window.setTimeout(fn) } };
-  if (typeof list=='function') list=[list];
-  if (list) for (var i=0; i<list.length; i++) window.setTimeout(list[i]);
-  else window.document.getElementById('main').innerHTML='JavaScript version not compatible (or loading error)!  <b>Sorry!</b>  Inkompatible JavaScript-Version (oder Ladefehler)!';
+  if (list) for (var i=0; i<list.length; i++) if (list[i]) window.setTimeout(list[i]);
+  else window.document.getElementById('main').innerHTML='JavaScript version not compatible or loading error!  <b>Sorry!</b>  Inkompatible JavaScript-Version oder Ladefehler!';
 })();
 
