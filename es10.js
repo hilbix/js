4c7a9acde00ac09d99037cfcc0ad4be1122bf51d
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-07-10 05:28:09 +0200

    WTF SHA256hex

--- es10.js
+++ es10.js
@@ -190,3 +190,13 @@ function arrayCmpShallow(a,b)
   return true;
 }
 
+// WTF https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+async function SHA256hex(message)
+{
+  const msgUint8 = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
