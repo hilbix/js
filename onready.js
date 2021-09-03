39984d8e4595d04ea402d50a77f6e949ee0fc437
Valentin Hilbig <webmaster@scylla-charybdis.com>
2021-09-03 17:24:22 +0200

    onready improved

--- onready.js
+++ onready.js
@@ -2,6 +2,7 @@
 // Compatible to idea presented in https://gist.github.com/shesek/441c02cf1f094c36f57a
 //
 // ;window.onready=[window.onready, YOUR_INIT_FUNCTION];
+// ;window.onready=[window.onready].push(YOUR_INIT_FUNCTION);
 //
 // If HTML is written this way, it ensures, that YOUR_INIT_FUNCTION is called when the DOM is ready:
 //
@@ -16,9 +17,19 @@
 // <script src="js/onready.js"></script></body></html>
 
 ;(function(window){
-  var n=0, ex=[], list=[window.onready];
-  function run(fn) { if (fn) window.setTimeout(function(x){ try { fn(x,ex) } catch (e) { ex.push([fn,x,e]) } }, 0, n++) };
-  window.onready = { push:run }
-  while (list.length) { var a = list.shift(); if (a instanceof Array) list = a.concat(list); else run(a); }
+  var n=0, ex=[];
+  run();
+  function run()
+    {
+      var fn, list=[window.onready]; window.onready=[];
+      while (list.length)
+        if (fn = list.shift())
+          if (fn instanceof Array) list=fn.concat(list);
+          else window.setTimeout(function(f,x) {
+            var e;
+            try { f(x,ex) } catch (e) { ex.push([f,x,e]) };
+            run()
+            }, 0, fn, n++)
+    }
 })(self || window || this);
 
