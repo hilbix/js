0a9fb7ac7a42419ff0666e648abf8354fb4ac406
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-07-05 05:15:21 +0200

    loadmain.js added

---
+++ loadmain.js
@@ -0,0 +1,51 @@
+'use strict';
+// APP.html:
+//
+// <!DOCTYPE html>
+// <html><head><meta charset="UTF-8"/>
+// <title>APP</title>
+// <link rel="stylesheet" type="text/css" href="APP.css"/>
+// <script src="er13.js" data-append="err"></script>
+// <script src="es13.js"></script>
+// <meta name="loadmodule" data-as="main" content="APP.js"/>
+// <meta name="loadmodule" data-as="lib"  content="APPLIB.js"/>
+// </head><body>
+// <div id="err"></div>
+// <div id="main">Please enable JavaScript</div>
+// <script type="module" src="loadmain.js"></script>
+// </body></html>
+//
+// APP.js:
+//
+// export class Main
+//   {
+//   constructor(_)
+//     {
+//       this.div = _;
+//     }
+//   async main(modules)
+//     {
+//       this.lib = await modules.lib;
+//       this.lib.hello_world(this.div);
+//     }
+//   }
+//
+// APPLIB.js:
+//
+// export function hello_world(e) { e.$text = 'hello world' }
+
+const _ = E('main').clr().text('initializing');
+try {
+  const { default:modules } = await import('./loadmodule')
+  console.log(_.$text = 'loading');
+  const main = await modules.main;
+  console.log(_.$text = 'loaded');
+  if (!main.Main) throw 'missing class Main';
+  const run = new main.Main(_);
+  if (!run.main) throw 'missing classfunction Main.main()';
+  run.main(modules);
+} catch (e) {
+  _.$text = `load failed: ${e}`;
+  throw e;
+}
+
