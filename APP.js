3bd62b63fb88ac3c6ee154a631a062223f810e5b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-07-05 05:15:21 +0200

    APP.js added

---
+++ APP.js
@@ -0,0 +1,15 @@
+'use strict'; // Example for loadmain.js
+
+export class Main
+  {
+  constructor(_)
+    {
+      this.div = _;
+    }
+  async main(modules)
+    {
+      this.lib = await modules.lib;
+      this.lib.hello_world(this.div);
+    }
+  }
+
